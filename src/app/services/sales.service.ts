d11b829df1fc5127a7d585867554f1dc1f2077c0
erickalonso98 <erick.edjaa@gmail.com>
2025-05-16 21:56:25 -0600

    se completo el crud de productos en el servicio productService y se agrego una nueva libreria de excel

--- src/app/services/sales.service.ts
+++ src/app/services/sales.service.ts
@@ -1,9 +1,43 @@
-import { Injectable } from '@angular/core';
-
+import { Injectable,inject } from '@angular/core';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { UserService } from './user.service';
+import { ISale } from '../models/Sale';
+import { global } from './global';
 @Injectable({
   providedIn: 'root'
 })
 export class SalesService {
 
-  constructor() { }
+  public url:string;
+
+  private _http = inject(HttpClient);
+  private _userService = inject(UserService);
+
+  constructor() {
+    this.url = global.url;
+   }
+
+   public Sales():Observable<ISale>{
+      let headers = this._userService.getHeaders();
+      return this._http.get<ISale>(`${this.url}/sales/`,{ headers });
+   }
+
+   public Sale(id:number):Observable<ISale>{
+      let headers = this._userService.getHeaders();
+      return this._http.get<ISale>(`${this.url}/sale/${id}`,{ headers });
+   }
+
+  public createSale(sale:any):Observable<ISale>{
+    let params = JSON.stringify(sale);
+    let headers = this._userService.getHeaders();
+    return this._http.post<ISale>(`${this.url}/sale/create-new-sale/`,params,{ headers });
+  }
+
+  public updateSale(sale:any,id:number):Observable<ISale>{
+    let params = JSON.stringify(sale);
+    let headers = this._userService.getHeaders();
+    return this._http.put<ISale>(`${this.url}/sale/updated-sales/${id}`,params,{ headers });
+  }
+   
 }
